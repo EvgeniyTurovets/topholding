9f960f2144cc7f8a5d12a1f919f3bc94a859d0ea
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-08-20 15:56:02 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -3,4 +3,20 @@ $(function(){
 		$(this).toggleClass('active')
 		$('.nav').fadeToggle()
 	})
+
+	$('.lang').click(function(){
+		$('.lang__wrap').fadeToggle(200)
+	})
+
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".lang__wrap"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+				$('.lang__wrap').fadeOut(200)
+		}
+	});
+
+
+	
+	
 })
